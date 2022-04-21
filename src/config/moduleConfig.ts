d023a8a2efd7e844080e474bcab3acd3cb1ef11d
alenjdev <alen@formant.io>
2022-04-21 14:06:59 -0500

    fix flexisoft nodes

--- src/config/moduleConfig.ts
+++ src/config/moduleConfig.ts
@@ -2,7 +2,7 @@ const nodes = [
   "/formant_bridge_node",
   "/ros_diagnostics_collector",
   "/rosout",
-  " /formant_ui/UI_Flexisoft_Status_node",
+  "/formant_ui/UI_Flexisoft_Status_node",
   "/formant_ui/UI_Mission_Status_node",
   "/formant_ui/UI_Vehicle_Status_node",
 ];
